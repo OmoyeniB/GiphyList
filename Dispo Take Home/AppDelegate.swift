a6bf401fe5aac33099609459484fb9923831c55f
SharonB9 <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>
2022-03-05 12:14:06 +0100

    User's interaction with keyboard handled

--- Dispo Take Home/AppDelegate.swift
+++ Dispo Take Home/AppDelegate.swift
@@ -1,9 +1,19 @@
 import UIKit
 import SnapKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      
+      IQKeyboardManager.shared.enable = true
+      IQKeyboardManager.shared.keyboardAppearance = .default
+      IQKeyboardManager.shared.toolbarDoneBarButtonItemText = "Dismiss"
+      IQKeyboardManager.shared.keyboardDistanceFromTextField = 50.0
+      IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+      IQKeyboardManager.shared.enableAutoToolbar = true
+      IQKeyboardManager.shared.shouldPlayInputClicks = false
+      
     return true
   }
 
