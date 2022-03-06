c681473cfc212cd361674d0b825638c5f2740e4e
SharonB9 <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>
2022-03-06 17:31:43 +0100

    updated Constants for search parameter

--- Dispo Take Home/Util/Constants.swift
+++ Dispo Take Home/Util/Constants.swift
@@ -6,6 +6,9 @@ extension Constants {
     // Get an API key from https://developers.giphy.com/dashboard/
     static let baseURL = "https://api.giphy.com/v1/gifs/"
     static let giphyApiKey = "lVKYZAM1Ni7MvPA613oBP1qwc7ihzgFU"
-    static let getTrendingGifURL = "\(baseURL)trending?api_key=\(giphyApiKey)&limit=25&trending"
+    static let searchRequiredParameter = "search?q="
+    static let getTrendingGifURL = "\(baseURL)trending?api_key=\(giphyApiKey)"
     static func getGifByIDURL(id: String) -> String {"\(baseURL)\(id)?api_key=\(giphyApiKey)"}
+    static func getGifSearched(q: String) -> String {"\(baseURL)\(searchRequiredParameter)\(q)&api_key=\(giphyApiKey)"}
 }
+
