8c73c29433f10b722181b8c22dcb255a60ccb878
SharonB9 <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>
2022-03-07 22:29:18 +0100

    popUpAlert created to handle error

---
+++ Dispo Take Home/View/UIViewController+Extension.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+extension UIViewController {
+    
+    public func popUpAlert(title: String, message: String, alertStyle: UIAlertController.Style,
+                           actionTitles: [String], actionStyles: [UIAlertAction.Style], actions: [((UIAlertAction) -> Void)]){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: alertStyle)
+        for(index, indexTitle) in actionTitles.enumerated() {
+            let action = UIAlertAction(title: indexTitle, style: actionStyles[index], handler: actions[index])
+            alertController.addAction(action)
+        }
+        self.present(alertController, animated: true)
+    }
+    
+    func ifNetworkIsOutOfCoverageDisplayErrorMessage() {
+        DispatchQueue.main.async {
+            self.popUpAlert(title: "Alert", message: "Oops!.. something went wrong. Please check your connection and try again", alertStyle: .alert,
+                       actionTitles: ["OK"], actionStyles: [.default], actions: [ { _ in
+                print("User out of network coverage")
+            }])
+        }
+    }
+}
