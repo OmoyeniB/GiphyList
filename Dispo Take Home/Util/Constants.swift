47903a67e7bc5b5baf352c1be37e3e5bbeccb3f8
SharonB9 <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>
2022-03-14 17:43:29 +0100

    constants text updated

--- Dispo Take Home/Util/Constants.swift
+++ Dispo Take Home/Util/Constants.swift
@@ -4,16 +4,20 @@ enum Constants {}
 
 extension Constants {
     
+    static let baseURL = "https://api.giphy.com/v1/gifs/"
+    static let giphyApiKey = "lVKYZAM1Ni7MvPA613oBP1qwc7ihzgFU"
+    static let getTrendingGifURL = "\(baseURL)trending?api_key=\(giphyApiKey)"
+    static func getGifByIDURL(id: String) -> String { "\(baseURL)\(id)?api_key=\(giphyApiKey)" }
+    static func getGifSearched(searchText:String) -> String { "\(baseURL)search?q=\(searchText)&api_key=\(giphyApiKey)" }
+    
+    static let popUpActionTitles = "OK"
+    static let popUpTitle = "Alert"
+    static let detailsTitle = "Gif Info Details"
     static let networkOutOfCoverage = "Oops!.. something went wrong. Please check your connection and try again"
     static let noSourceFound = "No source found"
     static let gifTitleRatingsLabel = "Rating:"
     static let gifSourceLabel = "Source:"
     static let titleLableText = "Title:"
     static let searchBarPlaceHolder = "Type to search ...."
-    static let baseURL = "https://api.giphy.com/v1/gifs/"
-    static let giphyApiKey = "lVKYZAM1Ni7MvPA613oBP1qwc7ihzgFU"
-    static let getTrendingGifURL = "\(baseURL)trending?api_key=\(giphyApiKey)"
-    static func getGifByIDURL(id: String) -> String { "\(baseURL)\(id)?api_key=\(giphyApiKey)" }
-    static func getGifSearched(searchText:String) -> String { "\(baseURL)search?q=\(searchText)&api_key=\(giphyApiKey)" }
     
 }
